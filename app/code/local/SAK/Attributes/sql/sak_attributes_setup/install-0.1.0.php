9ac10758ae1617b6b2f0bf153f9183447914cb35
Sergey Kovalchuk <sakovalchuk@tut.by>
2019-10-11 15:29:32 +0300

    start

---
+++ app/code/local/SAK/Attributes/sql/sak_attributes_setup/install-0.1.0.php
@@ -0,0 +1,55 @@
+<?php
+
+$installer = $this;
+
+$installer->startSetup();
+
+$installer->addAttribute('catalog_product', 'city_of_manufacture', array(
+    'group'           => 'General',
+    'label'           => 'City of manufacture',
+    'input'           => 'text',
+    'type'            => 'varchar',
+    'required'        => 0,
+    'visible_on_front'=> 1,
+    'filterable'      => 0,
+    'searchable'      => 0,
+    'comparable'      => 0,
+    'user_defined'    => 1,
+    'is_configurable' => 0,
+    'global'          => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'note'            => '',
+));
+
+$installer->addAttribute('catalog_product', 'dimentions', array(
+    'group'           => 'General',
+    'label'           => 'Dimentions',
+    'input'           => 'text',
+    'type'            => 'varchar',
+    'required'        => 0,
+    'visible_on_front'=> 1,
+    'filterable'      => 0,
+    'searchable'      => 0,
+    'comparable'      => 0,
+    'user_defined'    => 1,
+    'is_configurable' => 0,
+    'global'          => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'note'            => '',
+));
+
+$installer->addAttribute('catalog_product', 'volume', array(
+    'group'           => 'General',
+    'label'           => 'Volume',
+    'input'           => 'text',
+    'type'            => 'integer',
+    'required'        => 0,
+    'visible_on_front'=> 1,
+    'filterable'      => 0,
+    'searchable'      => 0,
+    'comparable'      => 0,
+    'user_defined'    => 1,
+    'is_configurable' => 0,
+    'global'          => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    'note'            => '',
+));
+
+$installer->endSetup();
